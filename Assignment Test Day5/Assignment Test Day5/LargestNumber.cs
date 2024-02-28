1c3ac0514ef6c46077c8141197bb03a6e9b1736a
Satyam Jaiswal <satyamj@infinite.com>
2024-02-28 12:32:08 +0530

    Submitting Assignment

---
+++ Assignment Test Day5/Assignment Test Day5/LargestNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Test_Day5
+{
+    class LargestNumber
+    {
+        public static void largest_num()
+        {
+            Console.Write("Enter three inputs: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"Largest number is: {a}");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine($"Largest number is: {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Largest number is: {c}");
+            }
+        }
+    }
+}
---
+++ Assignment Test Day5/Assignment Test Day5/RemoveChar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Test_Day5
+{
+    class RemoveChar
+    {
+        public static void remove_char()
+        {
+            Console.Write("Enter the String: ");
+            string s = Console.ReadLine();
+            Console.Write("Enter the position you want to remove: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0 && n > s.Length)
+            {
+                Console.WriteLine("Please Enter Valid Position");
+            }
+            else
+            {
+                s = s.Remove(n, 1);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
